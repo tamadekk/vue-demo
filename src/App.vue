42423cf1dc367de0027a41a8e6a1e5e65995dbe3
tamadekk <186jnd@gmail.com>
2025-07-13 23:31:28 +0200

    feat: project init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div>
+      <h1>Vue Demo</h1>
+      <p>Welcome to the Vue.js demo application!</p>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style></style>
